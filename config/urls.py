cefc165271533f6f9c995c21d68f5b6b548d9ad6
minerva <minerva.byj@gmail.com>
2024-11-13 01:47:59 +0800

    update: .gitignore

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-# config/urls.py
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
